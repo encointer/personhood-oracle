7cea8eb9c14764050b93052af498c1810c583fd5
Szilard Parrag <szilard.parrag@gmail.com>
2023-08-07 17:03:04 +0200

    nostr: followup regarding TimeSupplier -> TimeProvider trait renaming

--- enclave-runtime/src/rpc/nostr_utils.rs
+++ enclave-runtime/src/rpc/nostr_utils.rs
@@ -17,7 +17,7 @@
 use crate::{String, Vec};
 use itp_time_utils::{duration_now, now_as_secs, Duration};
 use log::info;
-use nostr::{prelude::*, types::time::TimeSupplier, Event, Timestamp};
+use nostr::{prelude::*, types::time::TimeProvider, Event, Timestamp};
 use tungstenite_sgx as tungstenite;
 
 use nostr::ClientMessage;
@@ -28,36 +28,36 @@ pub struct DemoTimeProvider;
 
 /// Time related operations are rather limited in `no_std` environments, which is why the `nostr`
 /// crate provides a trait that can help `no_std` users have time related operations.
-impl TimeSupplier for DemoTimeProvider {
+impl TimeProvider for DemoTimeProvider {
 	type Now = Duration;
 	type StartingPoint = i64;
 
-	fn instant_now(&self) -> <Self as TimeSupplier>::Now {
+	fn instant_now(&self) -> <Self as TimeProvider>::Now {
 		duration_now()
 	}
-	fn now(&self) -> <Self as TimeSupplier>::StartingPoint {
+	fn now(&self) -> <Self as TimeProvider>::StartingPoint {
 		unimplemented!()
 	}
 	fn duration_since_starting_point(
 		&self,
-		_: <Self as TimeSupplier>::StartingPoint,
+		_: <Self as TimeProvider>::StartingPoint,
 	) -> std::time::Duration {
 		unimplemented!()
 	}
-	fn starting_point(&self) -> <Self as TimeSupplier>::StartingPoint {
+	fn starting_point(&self) -> <Self as TimeProvider>::StartingPoint {
 		unimplemented!()
 	}
 	fn elapsed_instant_since(
 		&self,
-		_: <Self as TimeSupplier>::Now,
-		_: <Self as TimeSupplier>::Now,
+		_: <Self as TimeProvider>::Now,
+		_: <Self as TimeProvider>::Now,
 	) -> std::time::Duration {
 		unimplemented!()
 	}
 	fn elapsed_since(
 		&self,
-		_: <Self as TimeSupplier>::StartingPoint,
-		_: <Self as TimeSupplier>::StartingPoint,
+		_: <Self as TimeProvider>::StartingPoint,
+		_: <Self as TimeProvider>::StartingPoint,
 	) -> Duration {
 		unimplemented!()
 	}
@@ -69,15 +69,15 @@ impl TimeSupplier for DemoTimeProvider {
 	}
 }
 
-pub fn get_time_supplier() -> DemoTimeProvider {
+pub fn get_time_provider() -> DemoTimeProvider {
 	DemoTimeProvider {}
 }
 
 pub fn get_ts() -> Timestamp {
 	let now = duration_now();
-	let time_supplier = get_time_supplier();
+	let time_provider = get_time_provider();
 
-	time_supplier.to_timestamp(now)
+	time_provider.to_timestamp(now)
 }
 
 pub fn send_nostr_events(events_to_send: Vec<Event>, relay: &str) -> Result<(), String> {
